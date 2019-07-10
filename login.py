e4988b1f12389b34defbe26cf475b6294d278945
Afeedh Shaji <39899544+afeedh@users.noreply.github.com>
2019-07-10 09:44:56 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,26 +1,22 @@
 import pandas as pd
 import requests
 import time
-import random
-
 
 class login:
     logged_in = 0
-
+    URL = 'http://msftconnecttest.com'
+    
     def login(username, password):
-    list = ['http://msfconnect.com/redirect',
-            'http://msfconnecttest.com/redirect', 'http://msn.com/redirect']
+        while(1):
+            try:
+                r = requests.get(url = URL, verify=False)
+                break
+            except Exception as e:
+                #pass
+                print(e)
 
-    while(1):
-        try:
-            p = random.choice(list)
-            r = requests.get(p, verify=False)
-            break
-        except Exception as e:
-            pass
-
-    magic = r.url.split('?')[1]
-    r = requests.post('http://msfconnect.com/',
-                      {'username': username, 'password': password, 'magic': magic, '4Tredir': p})
-    if 'keepalive' in r.url:
-        logged_in = 1
+        magic = r.url.split('?')[1]
+        r = requests.post(url=URL,
+                          {'username': username, 'password': password, 'magic': magic, '4Tredir': URL})
+        if 'keepalive' in r.url:
+            logged_in = 1
